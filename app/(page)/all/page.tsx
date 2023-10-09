e89161d05ab725b8b02a4bf0d102bdbccb1ae949
vaggoban <143816541+vaggoban@users.noreply.github.com>
2023-10-09 22:01:20 +0600

    okay let me see

--- app/(page)/all/page.tsx
+++ app/(page)/all/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Gather_them from "./(page)/start_section/Gather_them"
 
 const page = () => {
   return (
     <div>
-      <Gather_them/>
-  
+        uuuuuuuuuuuuuuu
     </div>
   )
 }
