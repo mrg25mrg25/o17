2b2710fe839ed51ac982464372ff54023e33c632
vaggoban <143816541+vaggoban@users.noreply.github.com>
2023-10-17 18:54:22 +0600

    t6

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-"use client"
 import React from 'react'
-import Nav from "./(nicola)/Nav/page"
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Footer from "./(nicola)/Footer/page"
-import First_section from "./(nicola)/sections/first_section/page"
-import Second_section from "./(nicola)/sections/second_section/page"
-import Ok from "./(nicola)/ok/page"
+
 const page = () => {
   return (
-<BrowserRouter>
-<Nav/>
-<Ok/>
-<Routes>
-  <Route path='/'element={<First_section/>}  />
-  <Route path='/ok'element={<Ok/>} />
-</Routes>
-<Footer/>
-</BrowserRouter>
+    <div>
+      now
+    </div>
   )
 }
 
